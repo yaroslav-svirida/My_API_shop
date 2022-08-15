d38f5651fc41222fd62a12b0ce5d44a40949da0b
yaroslav-svirida <yaroslav.svirida@gmail.com>
2022-08-15 18:33:58 +0300

    Add celery beat (sorted by color)

---
+++ myAPIshop/celery.py
@@ -0,0 +1,52 @@
+"""
+Celery config file
+
+https://docs.celeryproject.org/en/stable/django/first-steps-with-django.html
+
+"""
+from __future__ import absolute_import
+import os
+from celery import Celery
+
+# this code copied from manage.py
+# set the default Django settings module for the 'celery' app.
+from myAPIshop import settings
+import logging
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myAPIshop.settings')
+
+app = Celery("myAPIshop")
+
+# read config from Django settings, the CELERY namespace would make celery
+# config keys has `CELERY` prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# load tasks.py in django apps
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+from django.apps import apps
+
+
+@app.task
+def select_brand():
+    Product = apps.get_model("myshop.Product")
+    products = Product.objects.all()
+    total = 0
+    for product in products:
+        if 'ATN' in product.article:
+            product.collection_name_id = 1
+            product.brend_name_id = 1
+            product.save()
+            logging.info(f'{product.id} DONE')
+            total += 1
+    logging.info(f"DONE {total}")
+
+
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'myshop.tasks.group_by_color',
+        'schedule': 10.0
+    },
+}
---
+++ myshop/tasks.py
@@ -0,0 +1,61 @@
+import logging
+
+from myAPIshop import app
+from myshop.models import Product
+
+
+@app.task
+def group_by_color():
+    products=Product.objects.all()
+    for product in products:
+        if 'ATN0001' in product.article:
+            product.colors_name_id = 1
+            product.save()
+            logging.error(f" 'ATN0001' {product.name}")
+
+        elif 'ATN0002' in product.article:
+            product.colors_name_id = 2
+            product.save()
+            logging.error(f" 'ATN0002' {product.name}")
+        elif 'ATN0003' in product.article:
+            product.colors_name_id = 3
+            product.save()
+            logging.error(f" 'ATN0003' {product.name}")
+        elif 'ATN0004' in product.article:
+            product.colors_name_id = 4
+            product.save()
+            logging.error(f" 'ATN0004' {product.name}")
+        elif 'ATN0005' in product.article:
+            product.colors_name_id = 5
+            product.save()
+            logging.error(f" 'ATN0005' {product.name}")
+        elif 'ATN0006' in product.article:
+            product.colors_name_id = 6
+            product.save()
+            logging.error(f" 'ATN0006' {product.name}")
+        elif 'ATN0007' in product.article:
+            product.colors_name_id = 7
+            product.save()
+            logging.error(f" 'ATN0007' {product.name}")
+        elif 'ATN0007' in product.article:
+            product.colors_name_id = 7
+            product.save()
+            logging.error(f" 'ATN0007' {product.name}")
+        elif 'ATN0008' in product.article:
+            product.colors_name_id = 8
+            product.save()
+            logging.error(f" 'ATN0008' {product.name}")
+        elif 'ATN0009' in product.article:
+            product.colors_name_id = 9
+            product.save()
+            logging.error(f" 'ATN0009' {product.name}")
+
+        elif 'ATN001' in product.article:
+            product.colors_name_id = 10
+            product.save()
+            logging.error(f" 'ATN001' {product.name}")
+
+
+
+
+
